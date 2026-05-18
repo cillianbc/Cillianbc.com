9c41341b1444a6353416ff1c4e383c8669263356
Cillian Bracken-Conway <cillianbc@users.noreply.github.com>
2026-05-18 17:33:01 +0100

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,1 @@
-export const BOOKING_URL = 'https://calendar.proton.me/u/1/bookings#_spVDmEIMZsc-eYxnUyAEBpUIR_sfFfbAcHmRFTqsnM=';
+export const BOOKING_URL = 'https://calendar.proton.me/bookings#weI8VtWD_NrVyzLd8PQMBc3tDUuWRBWdWVkJU4aTj34=';
